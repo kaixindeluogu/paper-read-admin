3c156ed1d003434529170d426db54cebb2e574d9
bmy <1129729148@qq.com>
2023-06-12 18:27:26 +0800

    测试

---
+++ back-stage-management/src/main/java/cn/tedu/back/stage/management/BackStageManagementApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.back.stage.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackStageManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackStageManagementApplication.class, args);
+    }
+
+}
---
+++ back-stage-management/src/test/java/cn/tedu/back/stage/management/BackStageManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.back.stage.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackStageManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
