171f60940fdb27203d4639e8673a4d6ba9c54e94
lee <1274666160@qq.com>
2023-06-16 19:59:11 +0800

    account

---
+++ back-stage-management/src/main/java/cn/tedu/back/stage/management/superadmin/account/dao/persist/mapper/userMapper.java
@@ -0,0 +1,8 @@
+package cn.tedu.back.stage.management.superadmin.account.dao.persist.mapper;
+
+/**
+ * Author = lee
+ * DATE   = 2023/6/16  19:48
+ */
+public class userMapper {
+}
---
+++ back-stage-management/src/main/java/cn/tedu/back/stage/management/superadmin/account/pojo/entity/User.java
@@ -0,0 +1,38 @@
+package cn.tedu.back.stage.management.superadmin.account.pojo.entity;
+
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * Author = lee
+ * DATE   = 2023/6/16  17:52
+ */
+@Data
+@Table(name = "zhipian_user", schema = "zhipian_shop")
+public class User implements Serializable {
+
+    @TableId(type =  IdType.AUTO)
+    private long id;
+    private Long userId;
+    private String userName;
+    private String password;
+    private String nickName;
+    private String admin;
+    private String eMail;
+    private String gender;
+    private String phoneNumber;
+    private Long idNumber;
+    private BigDecimal account;
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime gmtCreate;
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime gmtModified;
+
+
+}
